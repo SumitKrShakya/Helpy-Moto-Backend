8b82e6f6d6de59ce8a49d93f1ca8e92be6f188cf
Sumit Kumar Shakya <sumitshakya20101999@gmail.com>
2022-07-10 13:08:51 +0530

    ipdate

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose')
-require('dotenv').config({ path: "./Keys/.env" })
+require('dotenv').config()
 
 app.use(express.static('public'))
 
